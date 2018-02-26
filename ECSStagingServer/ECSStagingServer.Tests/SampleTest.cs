dde5e9033e169d44a17eb6969a19cb85e5280632
Scott Roberts <s.e.roberts0@gmail.com>
2018-02-26 00:19:38 -0800

    Project Setup

---
+++ ECSStagingServer/ECSStagingServer.Tests/SampleTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Moq;
+
+namespace ECSStagingServer.Tests
+{
+    public class SampleTest
+    {
+        public class SampleTestMethod
+        {
+            [Fact]
+            public void SampleTestCase()
+            {
+                // Arrange
+                
+                // Act
+                
+                // Assert
+            }
+        }
+    }
+}
